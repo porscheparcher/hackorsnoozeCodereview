6d4abea6c1669bf6bf5544ad37ad25385b5f8bff
Porsche Parcher <130427863+porscheparcher@users.noreply.github.com>
2023-08-02 13:10:35 -0600

    Update stories.js
    the favorites star is click and unclickable and also moves to the favorites section.

--- stories.js
+++ stories.js
@@ -28,15 +28,71 @@ function generateStoryMarkup(story) {
   return $(`
       
       <li id="${story.storyId}">
+      ${generateStarMarkup(story)}
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
         <small class="story-hostname">(${hostName})</small>
         <small class="story-author">by ${story.author}</small>
         <small class="story-user">posted by ${story.username}</small>
+        ${generateTrashMarkup(story.storyId)}
       </li>
     `);
 }
+function generateStarMarkup(story) {
+  let usersFavoriteStories = currentUser.favorites;
+  let foundStory = usersFavoriteStories.find(a_story => {
+    return a_story.id === story.id
+
+  })
+  if(foundStory) {
+    return `<span class = "fas fa-star"></span>`
+  }
+  else {
+    return `<span class = "far fa-star"></span>`
+  }
+
+}
+function generateTrashMarkup(storyId) {
+  let usersFavoriteStories = currentUser.favorites
+  let isCurrentUserFavorite = usersFavoriteStories.some(story => story.id === storyId);
+  if (isCurrentUserFavorite) {
+    return `<span class="fa fa-trash" data-story-id="${storyId}"></span>`;
+  }
+  return '';
+}
+
+/*const spans = document.getElementsByTagName('span');
+  /*spans.onclick = function() {
+    console.log('star was clicked')
+    this.classList.add('fas', 'fa-star')
+  }
+  for (let i = 0; i < spans.length; i++) {
+  spans[i].addEventListener('click', function() {
+    if (this.classList.contains('far')) {
+      this.classList.remove('far');
+      this.classList.add('fas');
+    } else {
+      this.classList.remove('fas');
+      this.classList.add('far')
+    }
+  });
+  spans[i].addEventListener('dblclick', function() {
+    if (this.classList.contains('fas')) {
+      this.classList.remove('fas');
+      this.classList.add('far')
+   
+    }
+  });
+  }
+/*const blankStar = document.getElementsByClassName('far fa-star');
+blankStar.addEventListener('click', function() {
+  blankStar.setAttribute('class', 'fas fa-star')
+      this.classList.remove('far');
+      this.classList.add('fas');
+})*/
+
+
 /**Handle the form submit when adding a new story */
 
 async function formSubmit() {
@@ -55,6 +111,7 @@ const submitEntry = document.getElementById('add-story');
 submitEntry.addEventListener('click', formSubmit)
 
 
+
 /** Gets list of stories from server, generates their HTML, and puts on page. */
 
 function putStoriesOnPage() {
@@ -65,24 +122,21 @@ function putStoriesOnPage() {
   // loop through all of our stories and generate HTML for them
   for (let story of storyList.stories) {
     const $story = generateStoryMarkup(story);
-    const favoriteStar = document.createElement('span')
-    favoriteStar.className = 'far fa-star';
-    $story.append(favoriteStar);
     $allStoriesList.append($story);
 
-    favoriteStar.addEventListener('click', function() {
-      favoriteStar.setAttribute('class', 'fas fa-star')
-      //this.classList.remove('far');
-      //this.classList.add('fas');
-
-      this.addEventListener('dblclick', function() {
-        if (this.classList.contains('fas')) {
-          this.classList.remove('fas');
-          this.classList.add('far')
-        }
-      })
-      })
-      
+    const starSpan = $story.find('span');
+    starSpan.on('click', function() {
+      if (starSpan.hasClass('far')) {
+        starSpan.removeClass('far').addClass('fas');
+      } else {
+        starSpan.removeClass('fas').addClass('far');
+      }
+    });
+    starSpan.on('dblclick', function() {
+      if (starSpan.hasClass('fas')) {
+        starSpan.removeClass('fas').addClass('far');
+      }
+    });   
   }
   $allStoriesList.show();
 }
@@ -111,6 +165,7 @@ mystories.addEventListener('click', function() {
   section.innerHTML = 'No Stories Added Yet!'
 
 })
+
 
 
 
